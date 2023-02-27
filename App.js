210c39ea510cc77e3503f008244e71a49558a574
MuhammadAmeen22 <111287688+MuhammadAmeen22@users.noreply.github.com>
2023-02-27 12:48:35 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Approuter from './config/Approuter';
+function App() {
+  return (
+    <div>
+      <Approuter/>
+    </div>
+  )
+}
+
+
+
+export default App;
